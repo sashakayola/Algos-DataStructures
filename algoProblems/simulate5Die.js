886cdf2651a31ea60f65d188110cbb26d37726fc
sashakayola <34871441+sashakayola@users.noreply.github.com>
2019-05-30 14:22:11 -0400

    Update simulate5Die.js

--- algoProblems/simulate5Die.js
+++ algoProblems/simulate5Die.js
@@ -1,5 +1,6 @@
-PROBLEM: given a 7 sided die, how can you simulate a 5 sided die (probabilities must be equal of 1-5)
-TIME: O(n)
+// PROBLEM: given a 7 sided die, how can you simulate a 5 sided die (probabilities must be equal of 1-5)
+// rand7() returns each integer with equal probability. rand5() must also return each integer with equal probability
+// TIME: O(n)
 
 function r7() {
   min = Math.ceil(1);
