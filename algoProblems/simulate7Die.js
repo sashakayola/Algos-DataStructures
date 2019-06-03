c812287cf736b5d87c88dc19522513180743373b
sashakayola <34871441+sashakayola@users.noreply.github.com>
2019-06-03 13:11:22 -0400

    Update simulate7Die.js

--- algoProblems/simulate7Die.js
+++ algoProblems/simulate7Die.js
@@ -1,20 +1,31 @@
 // PROBLEM: given a 5 sided die, how can you simulate a 7 sided die (probabilities must be equal of 1-7)
-// TIME: O(infinity) there is a very small chance that you will only get numbers above 5 when you roll a 7 but the chance of this is very small
+// TIME: O(infinity) (small chance if we re-roll forever)
 // SPACE: O(1) constant space
 
 function r5() {
   return Math.floor(Math.random() * (5 - 1)) + 1; 
 }
 
-// the only way to get 100% equal probabilities is to roll two dies, sum up the results. If the sum is gerater than 7, re-roll
-
-// used an iterative approach because with recursive approach, although cleaner, could result in a stack overflow
+// transform all 25 possible results from rolling two 5 sided die twice into a table where of the 25 possibilities, these translate to a number between 1-7
+// if the result is > 21 then re-roll
 function r7() {
+   const results = [
+    [1, 2, 3, 4, 5],
+    [6, 7, 1, 2, 3],
+    [4, 5, 6, 7, 1],
+    [2, 3, 4, 5, 6],
+    [7, 0, 0, 0, 0],
+  ];
+
   let roll1 = r5();
   let roll2 = r5();
-  while (roll1 + roll2 > 7) {
+
+  while (results[roll1][roll2] === 0) {
     let roll1 = r5();
     let roll2 = r5();
   }
-  return roll1 + roll2;
+
+  return results[roll1][roll2];
 }
+
+r7();
