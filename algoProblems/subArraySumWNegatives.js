63124d3a91dbfabc32dbd8957e95229dc1fe3978
sashakayola <34871441+sashakayola@users.noreply.github.com>
2019-06-05 08:47:12 -0400

    Create subArraySumWNegatives.js

---
+++ algoProblems/subArraySumWNegatives.js
@@ -0,0 +1,3 @@
+// PROBLEM: Given an unsorted array of integers, find a subarray which adds to a given number. If there are more than one subarrays with the sum as the given number, print any of them.
+// Handles negative numbers.
+
