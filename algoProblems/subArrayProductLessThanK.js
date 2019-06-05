0e354399b79b9b1b603411632bd8f23c4986e49d
sashakayola <34871441+sashakayola@users.noreply.github.com>
2019-06-05 14:30:36 -0400

    Update subArrayProductLessThanK.js

--- algoProblems/subArrayProductLessThanK.js
+++ algoProblems/subArrayProductLessThanK.js
@@ -36,6 +36,50 @@ function subarrayProducts(arr, k) {
   return subArrays.length;
 }
 
+// APPROACH 2: sliding window approach
+function subarrayProducts(arr, k) {
+  // start the window with the left point at index 0
+  let leftPointer = 0;
+
+  // keep track of the running product
+  let runningProduct = 1;
+
+  // the count is the number of valid subArrays
+  let count = 0;
+
+  // iterate through the array
+  for (let i = 0; i < arr.length; i++) {
+    let currentNum = arr[i];
+
+    // if the current number we are looking at is greater than k, move on (continue with the next number in the array)
+    if (currentNum < k) {
+      // if the current number is less than k, add 1 to the count
+      count++;
+      // recalculate the runningProduct now that our 'window' has increased in size by 1
+      runningProduct *= currentNum;
+    
+      // if the runningProduct is less than k, great. add the appropriate amount to the count (how large the window is)
+      if (runningProduct < k) {
+        count += (i - leftPointer);
+      }
+
+      // if the runningProduct is greater than k, than move the leftPointer over one to the right and recalculate the runningProduct (divide runningProduct by the value in the leftPointer)
+      else {
+        while (leftPointer < i && runningProduct >= k) {
+          runningProduct /= arr[leftPointer];
+          leftPointer++;
+        }
+        // once the runningProduct is less than k again, add the appropriate amount to the count (the size of the window)
+        count += (i - leftPointer);
+      }
+    }
+
+  }
+
+  return count;
+}
+
+
 // subarrayProducts([1,2,3,4,5], 6) // 7
 // subarrayProducts([3,4,5,12,20,60], 5) // 3
 // subarrayProducts([10,5,2,6],100) // 8
