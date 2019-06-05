a7db295aa8d071737cf7a4f68d2356a2d25e730d
sashakayola <34871441+sashakayola@users.noreply.github.com>
2019-06-05 11:39:51 -0400

    Update subArraySumWNegatives.js

--- algoProblems/subArraySumWNegatives.js
+++ algoProblems/subArraySumWNegatives.js
@@ -1,3 +1,20 @@
-// PROBLEM: Given an unsorted array of integers, find a subarray which adds to a given number. If there are more than one subarrays with the sum as the given number, print any of them.
+// PROBLEM: Given an unsorted array of integers, return true if you find a subarray which adds to a given number
 // Handles negative numbers.
 
+// APPROACH: iterate through the array and calculate the running sum. at each iteration, see if the runningSum - target is in the hash
+// if it is, return true. If not, add the running sum to the hash
+// TIME: O(n) because iterate through the array once
+function subarraySum(arr, target) {
+  let sumsMap = {0: 1}; // hash of previous sums
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+  	sum += arr[i];
+    if (sumsMap[sum - target]) {
+    	return true;
+    }
+    sumsMap[sum] = true;
+  }
+  return false;
+}
+
+subarraySum([10, 2, -2, -20, 10], -10)
