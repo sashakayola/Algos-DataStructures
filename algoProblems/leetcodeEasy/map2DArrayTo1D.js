3b50e2e35af56d9a267bad5f8921f70cc300d357
sashakayola <34871441+sashakayola@users.noreply.github.com>
2019-07-24 22:00:09 -0400

    Create map2DArrayTo1D.js

---
+++ algoProblems/leetcodeEasy/map2DArrayTo1D.js
@@ -0,0 +1,7 @@
+// PROBLEM: given the coordinates of a cell in a 2D array, return the index the cell would be in a 1D array
+(note: problem not on leetcode, but the difficulty is easy)
+// given the row of the cell and the column of the cell and the width of the 2D board
+
+function 2Dcoordinates(row, column, widthOfBoard) {
+  return row * widthOfBoard + column;
+}
