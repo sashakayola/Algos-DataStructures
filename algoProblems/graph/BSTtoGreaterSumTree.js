99d9be551bcf660b06900afa1823a2f70b77beb3
sashakayola <34871441+sashakayola@users.noreply.github.com>
2019-07-27 14:04:49 -0400

    Create BSTtoGreaterSumTree.js

---
+++ algoProblems/graph/BSTtoGreaterSumTree.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
+
+// DFS - post order (from largest value in the array to the smallest)
+// have an array where you keep track of the sums (essentially in this problem you just want to know the sum as you go)
+var bstToGst = function(root, arr = [0]) {
+    if (!root) {
+       return;
+    }
+    
+    bstToGst(root.right, arr);
+    let lastValue = arr[arr.length - 1];
+    arr.push(root.val + lastValue)
+    root.val = arr[arr.length - 1];
+    bstToGst(root.left, arr);
+   
+    return root;
+};
